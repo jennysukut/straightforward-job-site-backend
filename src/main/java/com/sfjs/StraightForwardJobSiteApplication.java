569c04b3105ecfb91e294189521c94e675cf9122
Carl Thronson <carlthronson@gmail.com>
2024-08-27 13:52:31 -0700

    Initial SpringBoot app - created with spring initializer tool website

---
+++ src/main/java/com/sfjs/StraightForwardJobSiteApplication.java
@@ -0,0 +1,13 @@
+package com.sfjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StraightForwardJobSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StraightForwardJobSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfjs/StraightForwardJobSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StraightForwardJobSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
