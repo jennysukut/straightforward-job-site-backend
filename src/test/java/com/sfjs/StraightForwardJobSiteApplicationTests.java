19518a62edd0ea64f03e14d388c8003f1790428d
Carl Thronson <carlthronson@gmail.com>
2024-08-27 14:17:18 -0700

    Format the default code generated by spring initializer

--- src/test/java/com/sfjs/StraightForwardJobSiteApplicationTests.java
+++ src/test/java/com/sfjs/StraightForwardJobSiteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StraightForwardJobSiteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
