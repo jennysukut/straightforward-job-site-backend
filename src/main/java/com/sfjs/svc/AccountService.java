c5e095e9e6bd7119ecf19209455e5d66138b4214
Carl Thronson <carlthronson@gmail.com>
2024-09-07 18:20:12 -0700

    Adding versioning requires some extra code for relationships

--- src/main/java/com/sfjs/svc/AccountService.java
+++ src/main/java/com/sfjs/svc/AccountService.java
@@ -1,11 +1,17 @@
 package com.sfjs.svc;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sfjs.entity.AccountEntity;
+import com.sfjs.entity.RoleEntity;
 import com.sfjs.repo.AccountRepository;
 import com.sfjs.repo.BaseRepository;
+import com.sfjs.repo.RoleRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -16,8 +22,24 @@ public class AccountService extends BaseService<AccountEntity> {
   @Autowired
   AccountRepository repository;
 
+  @Autowired
+  private RoleRepository roleRepository;
+
   @Override
   public BaseRepository<AccountEntity> getBaseRepository() {
     return this.repository;
+  }
+
+  @Override
+  public AccountEntity save(AccountEntity entity) {
+    if (entity.getRoles() != null) {
+      Set<RoleEntity> roles = entity.getRoles().stream().map(role -> {
+        return roleRepository.findById(role.getId()).orElseThrow(); // Need to load the role
+      }).collect(Collectors.toSet());
+      entity.setRoles(roles);
+    } else {
+      entity.setRoles(Collections.emptySet());
+    }
+    return super.save(entity);
   }
 }
