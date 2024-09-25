8d7358937280c8a0ced8771cc2816b03fd0fe9ce
Carl Thronson <carlthronson@gmail.com>
2024-09-25 12:25:34 -0700

    Fix CORS, make the allowed origins configurable

--- src/main/java/com/sfjs/WebConfig.java
+++ src/main/java/com/sfjs/WebConfig.java
@@ -1,16 +1,30 @@
 package com.sfjs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+  @Autowired
+  private Environment env;
+
+  private String allowedOrigins;
+
+  @PostConstruct
+  public void init() {
+    allowedOrigins = env.getProperty("spring.webflux.cors.allowed-origins");
+  }
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/graphql/**")
-      .allowedOrigins("http://localhost:3001")
+      .allowedOrigins(allowedOrigins)
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true)
