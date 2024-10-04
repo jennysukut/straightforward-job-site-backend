fa52b7663bdc632d5dcc58c21188b740120459ef
Carl Thronson <carlthronson@gmail.com>
2024-10-04 14:04:22 -0700

    Add security.  Require API_KEY to access API

--- src/main/java/com/sfjs/SecurityConfig.java
+++ src/main/java/com/sfjs/SecurityConfig.java
@@ -1,24 +1,33 @@
 package com.sfjs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  private AuthenticationFilter authenticationFilter;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-      .cors().and()
-      .csrf().disable()
-      .headers().frameOptions().sameOrigin().and()
-      .authorizeRequests()
-      .anyRequest().permitAll();
-
+    http.cors().and().csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+            .requestMatchers("/**").authenticated())
+        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
+
 }
--- src/main/java/com/sfjs/StraightForwardJobSiteApplication.java
+++ src/main/java/com/sfjs/StraightForwardJobSiteApplication.java
@@ -4,6 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import okhttp3.OkHttpClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @EnableJpaAuditing
 public class StraightForwardJobSiteApplication {
 
