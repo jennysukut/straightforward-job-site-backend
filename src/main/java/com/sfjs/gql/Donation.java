23fefaf313e96681c2943c833dd3fe9885715b14
Carl Thronson <carlthronson@gmail.com>
2024-10-04 15:50:54 -0700

    Add GQL query to get current donations total

--- src/main/java/com/sfjs/gql/Donation.java
+++ src/main/java/com/sfjs/gql/Donation.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -36,5 +37,10 @@ public class Donation {
   public Mono<Payment> acceptFellowDonation(@Argument(name = "donation") FellowDonation donation) {
     logger.info("Donation: " + donation);
     return service.acceptFellowDonation(donation);
+  }
+
+  @QueryMapping(name = "currentDonations")
+  public String currentDonations() {
+    return service.getCurrentDonations();
   }
 }
--- src/main/java/com/sfjs/svc/DonationService.java
+++ src/main/java/com/sfjs/svc/DonationService.java
@@ -1,11 +1,14 @@
 package com.sfjs.svc;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sfjs.dto.BusinessDonation;
 import com.sfjs.dto.FellowDonation;
 import com.sfjs.dto.Payment;
+import com.sfjs.entity.PaymentStatus;
 
 import jakarta.transaction.Transactional;
 import reactor.core.publisher.Mono;
@@ -31,4 +34,13 @@ public class DonationService {
     return checkoutService.acceptFellowDonation(donation);
   }
 
+  public String getCurrentDonations() {
+    BigDecimal currentDonations = BigDecimal.ZERO;
+    for (Payment payment: paymentService.findAll()) {
+      if (payment.getStatus().contentEquals(PaymentStatus.APPROVED.name())) {
+        currentDonations = currentDonations.add(new BigDecimal(payment.getAmount()));
+      }
+    }
+    return currentDonations.toString();
+  }
 }
