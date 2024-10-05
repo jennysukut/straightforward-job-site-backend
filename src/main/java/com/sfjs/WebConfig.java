b17c39fe45e906f72dc1c48d80326691b5545255
Carl Thronson <carlthronson@gmail.com>
2024-10-05 15:54:43 -0700

    Change application property settings for developer usage These values will be overridden in production settings

--- src/main/java/com/sfjs/WebConfig.java
+++ src/main/java/com/sfjs/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/graphql/**")
-      .allowedOrigins(allowedOrigins)
+      .allowedOriginPatterns(allowedOrigins)
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true)
