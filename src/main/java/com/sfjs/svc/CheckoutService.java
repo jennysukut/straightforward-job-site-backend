992d2f064b71a86d6b6d4f64fe8c86e7dd7d1760
Carl Thronson <carlthronson@gmail.com>
2024-10-08 15:02:19 -0700

    Use new signup service to signup fellow before initializing payment

--- src/main/java/com/sfjs/svc/CheckoutService.java
+++ src/main/java/com/sfjs/svc/CheckoutService.java
@@ -1,5 +1,6 @@
 package com.sfjs.svc;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +10,17 @@ import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.stereotype.Service;
 
+import com.sfjs.conv.PaymentConverter;
 import com.sfjs.dto.Business;
 import com.sfjs.dto.BusinessDonation;
 import com.sfjs.dto.Fellow;
 import com.sfjs.dto.FellowDonation;
 import com.sfjs.dto.Payment;
+import com.sfjs.entity.FellowEntity;
+import com.sfjs.entity.PaymentEntity;
 import com.sfjs.entity.PaymentStatus;
+import com.sfjs.repo.FellowRepository;
+import com.sfjs.repo.PaymentRepository;
 
 import jakarta.transaction.Transactional;
 import reactor.core.publisher.Mono;
@@ -37,6 +43,18 @@ public class CheckoutService {
 
   @Autowired
   FellowService fellowService;
+
+  @Autowired
+  SignupService signupService;
+
+  @Autowired
+  PaymentConverter paymentConverter;
+
+  @Autowired
+  FellowRepository fellowRepository;
+
+  @Autowired
+  PaymentRepository paymentRepository;
 
   Logger logger = Logger.getLogger(getClass().getName());
 
@@ -82,8 +100,14 @@ public class CheckoutService {
 
   public Mono<Payment> acceptFellowDonation(FellowDonation donation) {
 
-    logger.info("Starting initialize");
+    logger.info("Implicit fellow signup");
+    Fellow fellow = new Fellow();
+    fellow.setName(donation.getName());
+    fellow.setEmail(donation.getEmail());
+    fellow = signupService.signupFellow(fellow);
+
     Payment payment = new Payment();
+    payment.setFellow(fellow);
     payment.setAmount(donation.getAmount());
     payment.setCurrency("USD");
     payment.setEmail(donation.getEmail());
@@ -95,23 +119,24 @@ public class CheckoutService {
       // Save a field to the database
       return Mono.fromCallable(() -> {
         logger.info("fromCallable");
-        Fellow fellow = new Fellow();
-        fellow.setName(donation.getName());
-        fellow.setEmail(donation.getEmail());
-        Fellow savedFellow = fellowService.customSave(fellow);
 
         // update this one field from response from helcim service
         String rawToken = response.getSecretToken();
+        logger.info("Raw token: " + rawToken);
         String SALT = KeyGenerators.string().generateKey();
+        logger.info("Encryption SALT: " + SALT);
         payment.setSALT(SALT);
+        logger.info("Encryption password: " + PASSWORD);
         TextEncryptor encryptor = Encryptors.text(PASSWORD, SALT);
         String encryptedToken = encryptor.encrypt(rawToken);
         payment.setSecretToken(encryptedToken);
         // save the entity and return it
         logger.info("Save payment: " + payment);
-        Payment savedPayment = paymentService.customSave(payment);
-        savedPayment.setFellow(savedFellow);
-        return savedPayment;
+        PaymentEntity paymentEntity = paymentConverter.convertToEntity(payment);
+        Optional<FellowEntity> fellowEntity = fellowRepository.findById(payment.getFellow().getId());
+        paymentEntity.setFellow(fellowEntity.get());
+        PaymentEntity savedPaymentEntity = paymentRepository.save(paymentEntity);
+        return paymentConverter.convertToBody(savedPaymentEntity);
       }).map(anotherPayment -> {
         anotherPayment.setCheckoutToken(response.getCheckoutToken());
         return anotherPayment;
