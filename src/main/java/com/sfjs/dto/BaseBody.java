b9f7fd91541f4bf16c51dc8fbfdf8ecb38728d31
Carl Thronson <carlthronson@gmail.com>
2024-10-07 17:23:47 -0700

    Add JPA entity auditing fields to data transfer objects

--- src/main/java/com/sfjs/dto/BaseBody.java
+++ src/main/java/com/sfjs/dto/BaseBody.java
@@ -1,8 +1,16 @@
 package com.sfjs.dto;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +21,31 @@ public class BaseBody {
   @Getter
   @Setter
   private Long id;
+
+  @Getter
+  @Setter
+  @Version
+  private Long version;
+
+  @Getter
+  @Setter
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+  private LocalDateTime createdAt;
+
+  @Getter
+  @Setter
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+  private LocalDateTime updatedAt;
+
+  @Getter
+  @Setter
+  @Column(name = "deleted_at")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+  private LocalDateTime deletedAt;
 
   @Getter
   @Setter
