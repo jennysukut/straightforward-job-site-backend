03af5f473de023319e2da69a9ec5137df3c94794
Carl Thronson <carlthronson@gmail.com>
2024-10-30 15:10:42 -0700

    Convert Api Key request filter from GenericFilterBean to OncePerRequestFilter

--- src/main/java/com/sfjs/security/AuthenticationFilter.java
+++ src/main/java/com/sfjs/security/AuthenticationFilter.java
@@ -8,23 +8,21 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Service
-public class AuthenticationFilter extends GenericFilterBean {
+public class AuthenticationFilter extends OncePerRequestFilter {
 
   @Autowired
   private AuthenticationService authenticationService;
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+  public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws IOException, ServletException {
     try {
       Authentication authentication = authenticationService.getAuthentication((HttpServletRequest) request);
