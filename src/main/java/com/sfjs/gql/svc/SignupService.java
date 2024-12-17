e9f634555fe3660f211281039028c1808d5bf999
Carl Thronson <carlthronson@gmail.com>
2024-12-17 15:59:39 -0800

    Authorize account after business signup

--- src/main/java/com/sfjs/gql/svc/SignupService.java
+++ src/main/java/com/sfjs/gql/svc/SignupService.java
@@ -321,6 +321,7 @@ public class SignupService {
     newBusinessEntity.setContactName(requestBody.getContactName());
     newBusinessEntity.setEarlySignup(requestBody.getEarlySignup() != null ? requestBody.getEarlySignup() : false);
     BusinessEntity savedBusinessEntity = businessRepository.save(newBusinessEntity);
+    authorizationService.login(requestBody.getEmail(), requestBody.getPassword());
     return savedBusinessEntity;
   }
 
