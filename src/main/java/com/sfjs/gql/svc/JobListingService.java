a9018704a92f2c09fbfc5650e48a29d63a3ce388
Carl Thronson <carlthronson@gmail.com>
2024-12-18 12:02:46 -0800

    Assign all the fields of the job listing

--- src/main/java/com/sfjs/gql/svc/JobListingService.java
+++ src/main/java/com/sfjs/gql/svc/JobListingService.java
@@ -1,5 +1,6 @@
 package com.sfjs.gql.svc;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,15 +29,48 @@ public class JobListingService {
   private JobListingRepository jobListingRepository;
 
   public JobListingData saveJobListing(JobListingData requestBody, DataFetchingEnvironment environment) throws Exception {
+    if (requestBody.getJobNumber() != null) {
+      Optional<JobListingEntity> opt = jobListingRepository.findById(requestBody.getJobNumber());
+      if (opt.isPresent()) {
+        // Update existing entity
+        JobListingEntity entity = opt.get();
+        assignFields(requestBody, entity);
+        entity = jobListingRepository.save(entity);
+        JobListingData data = new JobListingData();
+        assignFields(entity, data);
+        data.setJobNumber(entity.getId());
+        return data;
+      } else {
+        // Existing entity not found
+        throw new IllegalArgumentException("Job listing is not found: " + requestBody.getJobNumber());
+      }
+    }
+    // Create new entity
     AccountEntity accountEntity = authorizationService.getAccount();
     BusinessEntity businessEntity = accountEntity.getBusiness();
     JobListingEntity entity = new JobListingEntity();
     entity.setBusiness(businessEntity);
-    // populate all fields
-    jobListingRepository.save(entity);
+    assignFields(requestBody, entity);
+    entity = jobListingRepository.save(entity);
     JobListingData data = new JobListingData();
+    assignFields(entity, data);
     data.setJobNumber(entity.getId());
-    // populate all fields
+    data.setBusinessName(businessEntity.getName());
     return data;
+  }
+
+  private void assignFields(JobListingData in, JobListingData out) {
+    out.setApplicationLimit(in.getApplicationLimit());
+    out.setCountry(in.getCountry());
+    out.setIdealCandidate(in.getIdealCandidate());
+    out.setJobTitle(in.getJobTitle());
+    out.setLocation(in.getLocation());
+    out.setLocationOption(in.getLocationOption());
+    out.setMoreAboutPosition(in.getMoreAboutPosition());
+    out.setNonNegParams(in.getNonNegParams());
+    out.setPerks(in.getPerks());
+    out.setPositionSummary(in.getPositionSummary());
+    out.setPositionType(in.getPositionType());
+    out.setPreferredSkills(in.getPreferredSkills());
   }
 }
