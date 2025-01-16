8db9393358f2e1b7f055ddd745371a31a1a5c5b6
vgajjar <vgajjar22@gmail.com>
2025-01-16 15:28:03 +0000

    add a resolver method for the allJobListings query

--- src/main/java/com/sfjs/gql/resolvers/JobListing.java
+++ src/main/java/com/sfjs/gql/resolvers/JobListing.java
@@ -3,6 +3,7 @@ package com.sfjs.gql.resolvers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +12,29 @@ import com.sfjs.gql.svc.JobListingService;
 
 import graphql.schema.DataFetchingEnvironment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 @Controller
 @Transactional
 public class JobListing {
 
+  Logger logger = Logger.getLogger(getClass().getName());
+
   @Autowired
   private JobListingService jobListingService;
+
+  @QueryMapping(name = "listAllJobs")
+  public List<JobListing> listAllJobs(){
+
+    List<JobListing> allJobs = new ArrayList<JobListing>();
+
+    logger.info("listAllJobs query executed...");
+
+
+    return allJobs;
+  }
 
   @MutationMapping(name = "saveJobListing")
   public void saveJobListing(@Argument(name = "requestBody") JobListingData requestBody,
