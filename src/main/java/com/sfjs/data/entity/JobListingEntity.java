6d6816d64852d468dfd4eb4ccbc2a54610079fbe
Carl Thronson <carlthronson@gmail.com>
2025-04-01 19:24:55 -0700

    Remove DecoratedJobListing class by using Transient annotation

--- src/main/java/com/sfjs/data/entity/JobListingEntity.java
+++ src/main/java/com/sfjs/data/entity/JobListingEntity.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.sfjs.data.core.DecoratedJobListing;
+import com.sfjs.data.core.JobListing;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
@@ -14,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,9 +27,13 @@ import lombok.Setter;
  */
 @Entity(name = "job_listing")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class JobListingEntity extends DecoratedJobListing {
+public class JobListingEntity extends JobListing {
 
   //@Getter @Setter HybridDetailsData hybridDetails; //?: any;
+  @Getter
+  @Setter
+  @Transient
+  private boolean saved;
 
   @Getter
   @Setter
