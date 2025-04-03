38b0a0b21ae0d7d19c667e093d2992895654c13d
Carl Thronson <carlthronson@gmail.com>
2025-04-03 11:05:15 -0700

    Create constants for Role names "BUSINESS" and "FELLOW"

--- src/main/java/com/sfjs/data/core/Role.java
+++ src/main/java/com/sfjs/data/core/Role.java
@@ -4,4 +4,7 @@ import com.sfjs.data.NamedBaseObject;
 
 public class Role extends NamedBaseObject {
 
+  public static final String BUSINESS = "BUSINESS";
+  public static final String FELLOW = "FELLOW";
+
 }
